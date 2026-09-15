7ac3a456209992263a1420223f1ad884edc76a4a
BW Movie Catalog <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-15 02:32:15 +0000

    Add/update movie: 337167

--- movies.js
+++ movies.js
@@ -6744,6 +6744,25 @@ const movies = [
     ]
   },
   {
+    "title": "Fifty Shades Freed",
+    "tmdbTitle": "Fifty Shades Freed",
+    "type": "movie",
+    "tmdbId": 337167,
+    "poster": "https://image.tmdb.org/t/p/w500/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg",
+    "year": "2018",
+    "runtime": "105m",
+    "genre": "Drama / Romance",
+    "rated": "R",
+    "director": "James Foley",
+    "cast": "Dakota Johnson, Jamie Dornan, Eric Johnson, Eloise Mumford, Rita Ora, Luke Grimes",
+    "synopsis": "Believing they have left behind shadowy figures from their past, newlyweds Christian and Ana fully embrace an inextricable connection and shared life of luxury. But just as she steps into her role as Mrs. Grey and he relaxes into an unfamiliar stability, new threats could jeopardize their happy ending before it even begins.",
+    "physical": [],
+    "digital": [
+      "Fandango",
+      "Movies Anywhere"
+    ]
+  },
+  {
     "title": "Fifty Shades of Grey",
     "tmdbTitle": "Fifty Shades of Grey",
     "type": "movie",
